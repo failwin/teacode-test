85699d20cd01e85ed03629bf0730ccf36c983ec3
Yuriy Kovalchuk <failwin@ukr.net>
2022-11-30 09:38:43 +0200

    add eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-
-  return (
-    <div className="App">
-      APP
-    </div>
-  )
+  return <div className="App">APP</div>;
 }
 
-export default App
+export default App;
